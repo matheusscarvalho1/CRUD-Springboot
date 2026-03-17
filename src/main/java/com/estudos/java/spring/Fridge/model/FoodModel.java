aab6bf4adf3e7b9317b19e5469ce3f0f27f4956c
Matheus de Souza Carvalho <06311697173@seguranca.local>
2026-03-17 12:17:00 -0400

    Feat: Usando Notations para criar getters e setters & construtores com Notations tambem automatico

--- src/main/java/com/estudos/java/spring/Fridge/model/FoodModel.java
+++ src/main/java/com/estudos/java/spring/Fridge/model/FoodModel.java
@@ -5,13 +5,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity //É uma entidade no meu banco de dados e irá criar uma tabela
-@Table(name="food_table") // Nome da tabela
+@Table(name = "food_table") // Nome da tabela
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class FoodModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,44 +32,45 @@ public class FoodModel {
     @jakarta.persistence.Column(nullable = false)
     private Integer quantity;
 
-    public FoodModel(Long id, String name, LocalDate expirationDate, Integer quantity) {
-        this.id = id;
-        this.name = name;
-        this.expirationDate = expirationDate;
-        this.quantity = quantity;
-    }
+    // @AllArgsConstructor
+    // public FoodModel(Long id, String name, LocalDate expirationDate, Integer quantity) {
+    //     this.id = id;
+    //     this.name = name;
+    //     this.expirationDate = expirationDate;
+    //     this.quantity = quantity;
+    // }
+    // GETTERS AND SETTERS
+        // public Long getId() {
+        //     return id;
+        // }
 
-    public Long getId() {
-        return id;
-    }
+        // public void setId(Long id) {
+        //     this.id = id;
+        // }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+        // public String getName() {
+        //     return name;
+        // }
 
-    public String getName() {
-        return name;
-    }
+        // public void setName(String name) {
+        //     this.name = name;
+        // }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+        // public LocalDate getExpirationDate() {
+        //     return expirationDate;
+        // }
 
-    public LocalDate getExpirationDate() {
-        return expirationDate;
-    }
+        // public void setExpirationDate(LocalDate expirationDate) {
+        //     this.expirationDate = expirationDate;
+        // }
 
-    public void setExpirationDate(LocalDate expirationDate) {
-        this.expirationDate = expirationDate;
-    }
+        // public Integer getQuantity() {
+        //     return quantity;
+        // }
 
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
+        // public void setQuantity(Integer quantity) {
+        //     this.quantity = quantity;
+        // }
 }
 
 
